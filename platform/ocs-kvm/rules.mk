b7349e5499e7095042657312f9cf5f4a506b6c1a
Hongyu Wu <hongyu.wu1@molex.com>
2026-07-20 15:38:15 -0700

    Create CLI filtering mechanism to mask packet switching CLIs for OCS devices.
    Adds a platform CLI command filter that prunes unsupported switch-oriented
    show/config/clear commands via the sonic-utilities plugin mechanism, driven
    by a per-device cli_unwanted.json blacklist:
    - platform/ocs-kvm/sonic-cli-filter: sonic-cli-filter deb (sonic_cli_filter.py
      core module, zzz_* show/config/clear plugins, debian packaging).
    - platform/ocs-kvm/sonic-cli-filter.mk/.dep and rules.mk/.dep/one-image.mk
      wiring to build and install the package into the ocs-kvm image.
    - device/virtual/x86_64-ocs-kvm_x86_64-r0/cli_unwanted.json: blacklist for
      the OCS-V device.

--- platform/ocs-kvm/rules.mk
+++ platform/ocs-kvm/rules.mk
@@ -3,6 +3,7 @@ include $(PLATFORM_PATH)/sai.mk
 include $(PLATFORM_PATH)/docker-syncd-ocs-kvm.mk
 include $(PLATFORM_PATH)/platform-modules-ocs-kvm.mk
 include $(PLATFORM_PATH)/sonic-version.mk
+include $(PLATFORM_PATH)/sonic-cli-filter.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/onie.mk
 include $(PLATFORM_PATH)/kvm-image.mk
